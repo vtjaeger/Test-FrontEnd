75ba76189a2aa7f7ec4d17ea1c791e61368fe957
vtjaeger <viniciustjaeger@gmail.com>
2024-02-06 15:17:03 -0300

    using html and js

---
+++ script.js
@@ -0,0 +1,39 @@
+const form = document.querySelector("form");
+
+const InputName = document.querySelector(".name-input");
+const InputEmail = document.querySelector(".email-input");
+const InputPassword = document.querySelector(".password-input");
+const InputTelephone = document.querySelector(".telephone-input");
+
+
+function cadastrar() {
+    fetch("http://localhost:8080/usuarios", {
+        headers: {
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+        } , 
+        method : "POST",
+        body: JSON.stringify({
+            name: InputName.value,
+            email : InputEmail.value,
+            password: InputPassword.value,
+            telephone: InputTelephone.value})
+    }) 
+    .then(function (res) {console.log(res)})
+    .catch(function (res) {console.log(res)})
+};
+
+function limparDados() {
+    InputName.value = "";
+    InputEmail.value = "";
+    InputPassword.value = "";
+    InputTelephone.value = ""; 
+}
+
+
+form.addEventListener('submit', function ( evento ) {
+    evento.preventDefault();
+
+    cadastrar();
+    limparDados();
+})
